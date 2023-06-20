4b068d3e6b95498a97bd18038d8f2b38229d97e1
PcLaboratorio <bruno.m.acerbi@edu.ufes.br>
2023-06-20 16:10:47 -0300

    update script

--- scriptTest.py
+++ scriptTest.py
@@ -7,6 +7,13 @@ def compile_program():
     subprocess.run(["flex", "scanner.l"])
     subprocess.run(["gcc", "-Wall", "-o", "compiler", "scanner.c", "parser.c", "tables.c", "types.c", "ast.c"])
 
+
+def delete_dot(path):
+    for file in os.listdir(path):
+        if file.endswith(".dot"):
+            complete_path = os.path.join(path, file)
+            os.remove(complete_path)
+
 def run_tests():
     # Leitura dos arquivos de entrada
     input_files = os.listdir("in")
@@ -23,10 +30,21 @@ def run_tests():
             output_file.write(result.stdout)
         
         print(f"Saida de {input_path} escrita em {output_path}")
+        
+
+        output_tree = os.path.join("treeOut", f"{file[:-2]}.dot")
+        output_pdf = os.path.join("treeOut", f"{file[:-2]}.pdf")
+        command_dot = f"./compiler < {input_path} 2> {output_tree}"
+        command_pdf = f"dot -Tpdf {output_tree} -o {output_pdf}"
+
+        subprocess.run(command_dot, shell=True, capture_output=True, text=True)
+        subprocess.run(command_pdf, shell=True, capture_output=True, text=True)        
+
 
 def main():
     compile_program()
     run_tests()
+    delete_dot("treeOut")
 
 if __name__ == "__main__":
     main()
